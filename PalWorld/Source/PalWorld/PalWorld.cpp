8595710ab98a2aeeddce82e6e80bc51b4835a7c2
KimTaeHyun <xoguszz123@naver.com>
2024-12-06 18:10:41 +0900

    프로젝트 추가

---
+++ PalWorld/Source/PalWorld/PalWorld.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PalWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PalWorld, "PalWorld" );
+ 
---
+++ PalWorld/Source/PalWorld/PalWorldGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PalWorldGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APalWorldGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APalWorldGameMode();
+};
+
+
+
